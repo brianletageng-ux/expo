e02e613e23e45caa04dde9a963922e4aa8e3ed66
Phil Pluckthun <phil@kitten.sh>
2026-02-10 15:56:40 +0000

    Reimplement env var reloading
    NOTE: This adds a new check that doesn't allow us to override env vars
    that haven't been loaded before by us, if they're already set.

--- packages/@expo/cli/src/start/server/metro/MetroBundlerDevServer.ts
+++ packages/@expo/cli/src/start/server/metro/MetroBundlerDevServer.ts
@@ -66,6 +66,7 @@ import { Log } from '../../../log';
 import { env } from '../../../utils/env';
 import { CommandError } from '../../../utils/errors';
 import { toPosixPath } from '../../../utils/filePath';
+import { reloadEnvFiles } from '../../../utils/nodeEnv';
 import { getFreePortAsync } from '../../../utils/port';
 import { BundlerDevServer, BundlerStartOptions, DevServerInstance } from '../BundlerDevServer';
 import {
@@ -1128,7 +1129,7 @@ export class MetroBundlerDevServer extends BundlerDevServer {
     }
 
     const envFiles = runtimeEnv
-      .getFiles(process.env.NODE_ENV)
+      .getEnvFiles({ mode: process.env.NODE_ENV })
       .map((fileName) => path.join(this.projectRoot, fileName));
 
     observeFileChanges(
@@ -1140,7 +1141,7 @@ export class MetroBundlerDevServer extends BundlerDevServer {
       () => {
         debug('Reloading environment variables...');
         // Force reload the environment variables.
-        runtimeEnv.load(this.projectRoot, { force: true });
+        reloadEnvFiles(this.projectRoot);
       }
     );
   }
